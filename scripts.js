0b370fde118880f152736fa5cde71c375e00944c
elanlavie <68617529+elanlavie@users.noreply.github.com>
2025-08-19 08:09:03 +0000

    fun cursor addition!

--- scripts.js
+++ scripts.js
@@ -20,3 +20,15 @@
     });
   }
 })();
+
+document.addEventListener("mousemove", e => {
+  const trail = document.createElement("div");
+  trail.className = "trail";
+  trail.style.left = `${e.pageX}px`;
+  trail.style.top = `${e.pageY}px`;
+  document.body.appendChild(trail);
+
+  setTimeout(() => {
+    trail.remove();
+  }, 600);
+});
